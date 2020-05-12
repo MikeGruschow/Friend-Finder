09f66ae8a50a086ba2314888e9d3c677134cdc95
Mike Gruschow <groosh91@gmail.com>
2020-05-12 00:03:37 -0400

    working

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-require("./app/routing/apiRoutes");
-require("./app/routing/htmlRoutes");
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 
