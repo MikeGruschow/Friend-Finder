a3d78bdc9ceb8870833ea35f0359eb319e6109f2
Mike Gruschow <groosh91@gmail.com>
2020-05-11 23:18:02 -0400

    heroku deployment

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+	
+	{
+		name: "Jon Snow",
+		photo: "https://pbs.twimg.com/profile_images/844785536195739651/GkGMChWu.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Daenerys Targaryen",
+		photo: "https://pbs.twimg.com/profile_images/737267065770348544/-_wliDDo.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Olenna Tyrell",
+		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/olenna-tyrell-512x512.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
+
